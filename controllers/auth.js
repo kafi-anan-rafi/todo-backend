9c7bc7a8278b9d58696dac7c7c73c3a6cbd7f02f
Kafi Anan Rafi <kafianan01@gmail.com>
2025-06-30 00:31:20 +0600

    Add user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
 import { User } from "../models/user.js";
 
 export async function Register(req, res) {
-  const { name, email, password } = req.body;
-  const user = await User.create({ name, email, password });
+  try {
+    const { name, email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+    const newUser = new User({ name, email, password });
+    newUser.save();
 
-  res.json({ name, email, password });
+    const safeUser = {
+      name: newUser.name,
+      email: newUser.email,
+    };
+
+    res.status(201).json({ message: "User registered!", user: safeUser });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to register user", error: err });
+  }
 }
