e887c85939967f4db0675bbab79b9ff55469dddc
Kafi Anan Rafi <kafianan01@gmail.com>
2025-07-01 01:51:52 +0600

    Add JWT generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,28 @@
 import { User } from "../models/user.js";
 import { hashPassword } from "../utils/bcrypt.js";
+import { generateToken } from "../utils/token.js";
 
 export async function Register(req, res) {
   try {
     const { name, email, password } = req.body;
-    const userExist = await User.findOne({ email });
+    const normalizedEmail = email.toLowerCase();
+
+    const userExist = await User.findOne({ email: normalizedEmail });
     if (userExist) {
       return res.status(400).json({ message: "User already exists!" });
     }
     const hashedPassword = await hashPassword(password);
-    const newUser = new User({ name, email, password: hashedPassword });
-    newUser.save();
+    const newUser = new User({
+      name,
+      email: normalizedEmail,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    const token = generateToken({ id: newUser._id });
 
     res.status(201).json({
       message: "User registered!",
-      user: {
-        name: newUser.name,
-        email: newUser.email,
-      },
+      token,
     });
   } catch (err) {
     res.status(500).json({ message: "Failed to register user", error: err });
