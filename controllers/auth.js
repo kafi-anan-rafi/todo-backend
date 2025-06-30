941db6d932e2cd9d7b0e019e8c1ebb4d3f2396f3
Kafi Anan Rafi <kafianan01@gmail.com>
2025-07-01 00:56:55 +0600

    Add password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.js";
+import { hashPassword } from "../utils/bcrypt.js";
 
 export async function Register(req, res) {
   try {
@@ -7,15 +8,17 @@ export async function Register(req, res) {
     if (userExist) {
       return res.status(400).json({ message: "User already exists!" });
     }
-    const newUser = new User({ name, email, password });
+    const hashedPassword = await hashPassword(password);
+    const newUser = new User({ name, email, password: hashedPassword });
     newUser.save();
 
-    const safeUser = {
-      name: newUser.name,
-      email: newUser.email,
-    };
-
-    res.status(201).json({ message: "User registered!", user: safeUser });
+    res.status(201).json({
+      message: "User registered!",
+      user: {
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
   } catch (err) {
     res.status(500).json({ message: "Failed to register user", error: err });
   }
