3c516fe22cf65cf18cbcb6f76a746184b08d18bf
Kafi Anan Rafi <kafianan01@gmail.com>
2025-07-04 12:13:25 +0600

    Enable cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import todoRoutes from "./routes/todos.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
+
 dotenv.config();
 
 await connectDB();
@@ -10,14 +12,16 @@ await connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Todo backend!" });
-});
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Todo backend!" });
+});
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}....`);
